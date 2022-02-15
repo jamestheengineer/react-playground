bef1c3c27bac68ad568f8589f2ea27490e184a47
jamestheengineer <jamestheengineer@gmail.com>
2022-02-15 18:05:22 -0500

    Update edit box with current selected to edit

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -6,17 +6,17 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item is from context',
+      text: 'This item is from context 1',
       rating: 10,
     },
     {
       id: 2,
-      text: 'This item is from context',
+      text: 'This item is from context 2',
       rating: 4,
     },
     {
       id: 3,
-      text: 'This item is from context',
+      text: 'This item is from context 3',
       rating: 2,
     },
   ]);
@@ -62,6 +62,7 @@ export const FeedbackProvider = ({ children }) => {
         deleteFeedback,
         addFeedback,
         editFeedback,
+        feedbackEdit,
       }}
     >
       {children}
