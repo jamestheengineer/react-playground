19cef9d8da0cf9b188bf48371a34a506df48f2ff
jamestheengineer <jamestheengineer@gmail.com>
2022-05-17 18:57:34 -0400

    Finish last useRef example

--- react-sandbox/src/components/Todo.jsx
+++ react-sandbox/src/components/Todo.jsx
@@ -2,16 +2,26 @@ import { useState, useEffect, useRef } from 'react'
 function Todo() {
   const [loading, setLoading] = useState(true)
   const [todo, setTodo] = useState({})
+
+  const isMounted = useRef(true)
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then((rest) => rest.json())
+      .then((res) => res.json())
       .then((data) => {
         setTimeout(() => {
-          setTodo(data)
-          setLoading(false)
+          if (isMounted.current) {
+            setTodo(data)
+            setLoading(false)
+          }
         }, 3000)
       })
-  }, [])
+
+    // Runs when unmounted
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
   return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>
 }
 
