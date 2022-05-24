01434ab7ff4c154884be7c8dcde51d7e382d237d
jamestheengineer <jamestheengineer@gmail.com>
2022-05-23 20:41:29 -0400

    Hooks

---
+++ react-sandbox/src/components/hooks/useFetch.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react'
+function useFetch() {
+  const [loading, setLoading] = useState(true)
+  const [error, SetError] = useState(null)
+  const [data, setData] = useState(null)
+  useEffect(() => {})
+}
+export default useFetch
