24071a77d336bf98653ad96c7dc284c3e9aac26f
jamestheengineer <jamestheengineer@gmail.com>
2022-05-24 18:49:34 -0400

    Custom hook example

---
+++ react-sandbox/src/components/CustomHookExample1.jsx
@@ -0,0 +1,6 @@
+import useFetch from './hooks/useFetch'
+
+function CustomHookExample1() {
+  return <div>CustomHookExample1</div>
+}
+export default CustomHookExample1
--- react-sandbox/src/components/hooks/useFetch.js
+++ react-sandbox/src/components/hooks/useFetch.js
@@ -1,8 +1,23 @@
 import { useState, useEffect } from 'react'
-function useFetch() {
+function useFetch(url, options) {
   const [loading, setLoading] = useState(true)
-  const [error, SetError] = useState(null)
+  const [error, setError] = useState(null)
   const [data, setData] = useState(null)
-  useEffect(() => {})
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options)
+        const data = await response.json()
+        setData(data)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return { data, loading, error }
 }
 export default useFetch
