7a306a08962bcc73a4285618a47b59209fcce77e
jamestheengineer <jamestheengineer@gmail.com>
2022-06-03 18:34:15 -0400

    Add new components

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,3 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/Signin'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+
 function App() {
   return (
     <>
