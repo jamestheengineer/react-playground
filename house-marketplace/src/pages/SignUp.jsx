04fa73625bfacea4d944a722273efe6289a0bc30
jamestheengineer <jamestheengineer@gmail.com>
2022-06-28 18:07:06 -0400

    Add toastify messages

--- house-marketplace/src/pages/SignUp.jsx
+++ house-marketplace/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -43,7 +44,7 @@ function SignUp() {
       await setDoc(doc(db, 'users', user.uid), formDataCopy)
       navigate('/')
     } catch (error) {
-      console.log(error)
+      toast.error('Something went wrong with registration')
     }
   }
   return (
