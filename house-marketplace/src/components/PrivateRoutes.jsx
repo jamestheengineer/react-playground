e6a2902c4230d44f5cbb78b7290c2cd39ac480a2
jamestheengineer <jamestheengineer@gmail.com>
2022-07-05 17:40:46 -0400

    Check if logged in

--- house-marketplace/src/components/PrivateRoutes.jsx
+++ house-marketplace/src/components/PrivateRoutes.jsx
@@ -1,7 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
 
 const PrivateRoutes = () => {
-  const loggedIn = false
+  const { loggedIn, checkingStatus } = useAuthStatus()
+
+  if (checkingStatus) {
+    return <h3>Loading..</h3>
+  }
+
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
