67074b4cb3087e096861409be95b96495e9c175e
jamestheengineer <jamestheengineer@gmail.com>
2022-07-06 18:03:27 -0400

    Add spinner component

--- house-marketplace/src/components/PrivateRoutes.jsx
+++ house-marketplace/src/components/PrivateRoutes.jsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 
 const PrivateRoutes = () => {
   const { loggedIn, checkingStatus } = useAuthStatus()
 
   if (checkingStatus) {
-    return <h3>Loading..</h3>
+    return <Spinner />
   }
 
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
