ebfb91b4e445ffce1e1d93dbd269c66001bebce0
jamestheengineer <jamestheengineer@gmail.com>
2022-07-10 18:46:00 -0400

    Keep adding google auth

--- house-marketplace/src/components/OAuth.jsx
+++ house-marketplace/src/components/OAuth.jsx
@@ -1,4 +1,21 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { doc, setDoc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import googleIcon from '../assets/svg/googleIcon.svg'
+
 function OAuth() {
-  return <div>OAuth</div>
+  const navigate = useNavigate()
+  const location = useLocation()
+  const onGoogleClick = () => {}
+  return (
+    <div className='socialLogin'>
+      <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with </p>
+      <button className='socialIconDiv' onClick={onGoogleClick}>
+        <img className='socialIconImg' src={googleIcon} alt='google' />
+      </button>
+    </div>
+  )
 }
 export default OAuth
