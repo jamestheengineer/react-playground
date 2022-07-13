e32d762c327430962ef67f9c90be5699471ffb08
jamestheengineer <jamestheengineer@gmail.com>
2022-07-13 18:01:36 -0400

    Query the listings from firebase

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -9,7 +9,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
-import Categories from './pages/Categories'
+import Category from './pages/Category'
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/category/:categoryName' element={<Categories />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoutes />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
--- house-marketplace/src/pages/Categories.jsx
+++
@@ -1,4 +0,0 @@
-function Categories() {
-  return <div>Categories</div>
-}
-export default Categories
