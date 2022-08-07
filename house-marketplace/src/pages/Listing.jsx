fe68e865b46ed97a873340d66cb2dbb3b1013204
jamestheengineer <jamestheengineer@gmail.com>
2022-08-06 21:13:42 -0400

    Add share link

--- house-marketplace/src/pages/Listing.jsx
+++ house-marketplace/src/pages/Listing.jsx
@@ -30,6 +30,23 @@ function Listing() {
     fetchListing()
   }, [navigate, params.listingId])
 
-  return <div>Listing</div>
+  return (
+    <main>
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+      >
+        <img src={shareIcon} alt='' />
+      </div>
+
+      {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+    </main>
+  )
 }
 export default Listing
